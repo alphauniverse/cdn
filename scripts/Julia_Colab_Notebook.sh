0218e2230473fdf06142d5b3b1f9865fbebf679b
alphauniverse <66740112+alphauniverse@users.noreply.github.com>
2021-12-02 14:34:07 +0800

    Create Julia_Colab_Notebook.sh

---
+++ scripts/Julia_Colab_Notebook.sh
@@ -0,0 +1,39 @@
+set -e
+
+#---------------------------------------------------#
+JULIA_VERSION="1.7.0" # any version ≥ 0.7.0
+JULIA_PACKAGES="IJulia BenchmarkTools Plots"
+JULIA_PACKAGES_IF_GPU="CUDA" # or CuArrays for older Julia versions
+JULIA_NUM_THREADS="auto"
+#---------------------------------------------------#
+
+if [ -n "$COLAB_GPU" ] && [ -z `which julia` ]; then
+  # Install Julia
+  JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
+  echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+  BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
+  URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
+  wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
+  tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
+  rm /tmp/julia.tar.gz
+
+  # Install Packages
+  if [ "$COLAB_GPU" = "1" ]; then
+      JULIA_PACKAGES="$JULIA_PACKAGES $JULIA_PACKAGES_IF_GPU"
+  fi
+  for PKG in `echo $JULIA_PACKAGES`; do
+    echo "Installing Julia package $PKG..."
+    julia -e 'using Pkg; pkg"add '$PKG'; precompile;"' &> /dev/null
+  done
+
+  # Install kernel and rename it to "julia"
+  echo "Installing IJulia kernel..."
+  julia -e 'using IJulia; IJulia.installkernel("julia", env=Dict(
+      "JULIA_NUM_THREADS"=>"'"$JULIA_NUM_THREADS"'"))'
+  KERNEL_DIR=`julia -e "using IJulia; print(IJulia.kerneldir())"`
+  KERNEL_NAME=`ls -d "$KERNEL_DIR"/julia*`
+  mv -f $KERNEL_NAME "$KERNEL_DIR"/julia  
+
+  echo ''
+  echo "Success! Please reload this page and jump to the next section."
+fi
